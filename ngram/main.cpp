c6a60fac0a07f5bb6d69a224d8c676b31da726ac
Axel Boldt-Christmas <xmas1915@gmail.com>
2014-10-16 16:55:55 +0200

    Initial Commit

---
+++ ngram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ngram
+//
+//  Created by Axel Boldt-Christmas on 16/10/14.
+//  Copyright (c) 2014 HAL9000. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
