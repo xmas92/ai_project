f2594524d2ebc4cd37dd03404b6225bba9178c74
Axel Boldt-Christmas <xmas1915@gmail.com>
2014-10-16 21:57:28 +0200

    Main file for testing

--- ngram/main.cpp
+++ ngram/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include "ngram.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Prefix<3> p;
+    p.Next("A");
+    p.Next("B");
+    p.Next("C");
+    auto p1 = p.Reduce();
+    auto p2 = p.Reduce().Reduce();
+    std::cout << p.First() << std::endl;
+    std::cout << p1.First() << std::endl;
+    std::cout << p2.First() << std::endl;
+    
+    NGram<3> _3gram;
+    std::istringstream iss ("Lets test and see if we can make a simple 3 gram.");
+    
+    iss >> _3gram;
+    
     return 0;
 }
