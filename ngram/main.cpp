86f2862fc7c72ec588393b285f515070dbcd24a5
Axel Boldt-Christmas <xmas1915@gmail.com>
2014-10-26 21:26:18 +0100

    Dummy Test File Commit

--- ngram/main.cpp
+++ ngram/main.cpp
@@ -9,25 +9,32 @@
 #include <iostream>
 #include <sstream>
 #include "ngram.h"
+#include "topk.h"
+#include "prefix.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    Prefix<3> p;
-    p.Next("A");
-    p.Next("B");
-    p.Next("C");
-    auto p1 = p.Reduce();
-    auto p2 = p.Reduce().Reduce();
-    std::cout << p.First() << std::endl;
-    std::cout << p1.First() << std::endl;
-    std::cout << p2.First() << std::endl;
-    
     NGram<3> _3gram;
     std::istringstream iss ("Lets test and see if we can make a simple 3 gram. Can you find a simple 3 gram?");
-    
     iss >> _3gram;
     
-    std::cout << _3gram;
+    TopK<2> top2(_3gram);
+    
+    // std::cout << _3gram;
+    std::cout << top2;
+    
+    Prefix<2> p;
+    p.Next("simple");
+    std::cout << "simple:" << std::endl;
+    for (auto s : top2.GetTopK(p)) {
+        if (s.empty()) break;
+        std::cout << s << std::endl;
+    }
+    p.Next("3");
+    std::cout << "simple 3:" << std::endl;
+    for (auto s : top2.GetTopK(p)) {
+        if (s.empty()) break;
+        std::cout << s << std::endl;
+    }
     
     return 0;
 }
